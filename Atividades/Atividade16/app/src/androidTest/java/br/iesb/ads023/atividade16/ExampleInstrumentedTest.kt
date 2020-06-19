b709a92538f4ac615095b0bba22c726d663294b1
Lucas29lms <lucas.moreira29@gmail.com>
2020-06-19 19:13:31 -0300

    Subindo atividades

--- Atividades/Atividade16/app/src/androidTest/java/br/iesb/ads023/atividade16/ExampleInstrumentedTest.kt
+++ Atividades/Atividade16/app/src/androidTest/java/br/iesb/ads023/atividade16/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.iesb.ads023.atividade17", appContext.packageName)
+        assertEquals("br.iesb.ads023.atividade16", appContext.packageName)
     }
 }
